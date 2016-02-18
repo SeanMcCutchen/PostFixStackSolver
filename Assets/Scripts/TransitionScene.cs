27043cde281d8c3ecea0da8a2a4c3bcc5d6dcd87
jsiegal94 <jsiegal@CS-XENA19.trinity.local>
2016-02-18 10:37:29 -0600

    Scene Transitions

---
+++ Assets/Scripts/TransitionScene.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransitionScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void loadDemoMode() {
+		Application.LoadLevel ("DemoMode");
+	}
+
+	public void loadPracticeMode() {
+		Application.LoadLevel ("PracticeMode");
+	}
+
+
+	public void loadMainMenu() {
+		Application.LoadLevel ("mainmenu");
+	}
+	/*
+	public void loadOptions() {
+		Application.LoadLevel ("Options");
+	}
+
+	public void loadLearningMode() {
+		Application.LoadLevel ("LearningMode");
+	}
+
+	public void loadPauseScreen () {
+		Application.LoadLevel ("PauseMenu");
+	}
+*/
+}
