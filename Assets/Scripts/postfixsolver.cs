85858b6056cca83e5af56190e510d0ca4c3e1db4
Sean McCutchen <smccutch@CS-XENA21.trinity.local>
2016-02-18 10:56:44 -0600

    solver

---
+++ Assets/Scripts/postfixsolver.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class postfixsolver : MonoBehaviour {
+
+	int solve(string postfix){
+				postfix = postfix.Trim();
+                string[] ans = postfix.Split(' ');
+                Stack<int> eval = new Stack<int>();
+                for (int x = 0; x < ans.Length; x++)
+                {
+                    if ("*+%/-".Contains(ans[x]))
+                    {
+                        int temp1;
+                        int temp2;
+
+                        switch (ans[x])
+                        {
+                            case ("*"):
+                                eval.Push(eval.Pop() * eval.Pop());
+                                break;
+                            case "-":
+                                temp1 = eval.Pop();
+                                temp2 = eval.Pop();
+                                eval.Push(temp2 - temp1);
+                                break;
+                            case "%":
+                                temp1 = eval.Pop();
+                                temp2 = eval.Pop();
+                                eval.Push(temp2 % temp1);
+                                break;
+                            case "+":
+                                eval.Push(eval.Pop() + eval.Pop());
+                                break;
+                            case "/":
+                                temp1 = eval.Pop();
+                                temp2 = eval.Pop();
+                                eval.Push(temp2 / temp1);
+                                break;
+                        }
+
+                    }
+                    else
+                        eval.Push(System.Int32.Parse(ans[x]));
+                }
+				int answer = eval.Pop();
+		return answer;
+}
+}
