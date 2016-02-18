596c6354b8eb2302cbc0935527e6bb13ee5ddff5
Sean McCutchen <smccutch@CS-XENA21.trinity.local>
2016-02-18 11:00:26 -0600

    finish postfixstack adding

--- Assets/Scripts/PostFixEval.cs
+++ Assets/Scripts/PostFixEval.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PostFixEval : MonoBehaviour {
-
+	string problem1 = "[(2+4)+3*(4/2)";
+	string problem2 = "[2*(6/2)+(3^2)]";
+	Stack<string> answer = new Stack<string>();
 	// Use this for initialization
 	void Start () {
 	
@@ -12,4 +15,8 @@ public class PostFixEval : MonoBehaviour {
 	void Update () {
 	
 	}
+	void stepThrough (){
+	}
+
+
 }
