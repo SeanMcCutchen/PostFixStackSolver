c2bc77b54a827c40154577c5c26d42ccc20b1b1a
Sean McCutchen <smccutch@trinity.edu>
2016-02-22 23:40:28 -0600

    Loads both problems now, only thing left is show popping, gotta ask hicks bout that

--- Assets/Scripts/PostFixEval.cs
+++ Assets/Scripts/PostFixEval.cs
@@ -7,9 +7,12 @@ public class PostFixEval : MonoBehaviour {
 	string problem2 = "[2*(6/2)+(3^2)]";
 	Stack<char> main = new Stack<char>();
 	Stack<char> trash = new Stack<char>();
+	int [] priorities = new int[100];
 	string numbers = "0123456789";
 	string expressions = "+-/*^()[]";
 	int x = 0;
+	char [] prob;
+	bool which = false;
 	
 	int countop,countcp,countobr, countcbr;
 	// Use this for initialization
@@ -25,30 +28,68 @@ public class PostFixEval : MonoBehaviour {
 	
 	}
 	public void stepThrough (){
-		Debug.Log (x + "x");
-		Debug.Log (countop + "countop");
-		Debug.Log (countcp + "countcp");
-		Debug.Log (countobr + "countobr");
-		Debug.Log (countcbr + "countcbr");
-		char [] prob1 = problem1.ToCharArray ();
 
-		Debug.Log (prob1.Length + "length");
-
-	
-		if(x<prob1.Length)
-		{
-			if(numbers.Contains(prob1[x]+""))
-			   trash.Push(prob1[x]);
-			else if(expressions.Contains(prob1[x]+""))
-				main.Push(prob1[x]);
-			                            
-
+		if(which==false)
+		load1 ();
+		else
+		load2 ();
+	}
+	public void load1 (){
+		prob = problem1.ToCharArray ();
+		if (x < prob.Length) {
+			if (numbers.Contains (prob[x] + ""))
+				trash.Push (prob[x]);
+			else if (expressions.Contains(prob [x] + ""))
+				main.Push (prob [x]);
+			
+			
 		}
-		if ((x == prob1.Length - 1)&&((countop!=countcp)||(countobr!=countcbr))) {
-			Debug.Log ("Invalid entry");
+		if (x == prob.Length - 1) {
+			
+			if ((countop != countcp) || (countobr != countcbr)) {
+				Debug.Log ("Invalid entry");
+			}
+			else{
+				Debug.Log ("Works");
+			}
+		}
+		if (x == prob.Length) {		
+			switchproblem();
+		}
+			
+		x++;
+	}
+	public void load2 (){
+
+		if (x < prob.Length) {
+		
+			if (numbers.Contains (prob [x] + ""))
+				trash.Push (prob [x]);
+			else if (expressions.Contains (prob [x] + ""))
+				main.Push (prob [x]);
+			
+			
+		}
+		if (x == prob.Length - 1) {
+			
+			if ((countop != countcp) || (countobr != countcbr)) {
+				Debug.Log ("Invalid entry");
+			} else {
+				Debug.Log ("Works");
+			}
+
 		}
 		x++;
 	}
-
-
+	void switchproblem(){
+		x = 0;
+		trash.Clear ();
+		main.Clear ();
+		prob = problem2.ToCharArray ();
+		countop = problem2.Split ('(').Length - 1;
+		countcp = problem2.Split (')').Length - 1;
+		countobr = problem2.Split ('[').Length - 1;
+		countcbr = problem2.Split (']').Length - 1;
+		which = true;
+	}
 }
